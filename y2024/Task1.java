1f45332a5cbe5c3421eccdd1e3d8d8e0252088da
Calvin Liusnando <calvin.liusnando@s2021.tu-chemnitz.de>
2024-12-07 15:13:59 +0100

    upload AoC 2024 Task1

--- y2024/Task1.java
+++ y2024/Task1.java
@@ -7,22 +7,26 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class Task1b {
+public class Task1 {
     public static void main(String[] args)
     {
-        int total = 0;
-        int leftList[] = new int[1000];
+        int total1a = 0;
+        int[] arr1 = new int[1000];
+        int[] arr2 = new int[1000];
+
+        int total1b = 0;
         int idx = 0;
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         File input = new File("./y2024/input.txt");
         try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
-            String line = "";
+            String line;
             while ((line = reader.readLine()) != null) {
                 String[] numbers = line.split(" ");
                 numbers = Arrays.stream(numbers)
                         .filter(value -> !value.isEmpty())
                         .toArray(String[]::new);
-                leftList[idx] = Integer.parseInt(numbers[0]);
+                arr1[idx] = Integer.parseInt(numbers[0]);
+                arr2[idx] = Integer.parseInt(numbers[1]);
                 int key = Integer.parseInt(numbers[1]);
                 if (hashMap.containsKey(key)){
                     hashMap.put(key, hashMap.get(key) + 1);
@@ -30,14 +34,25 @@ public class Task1b {
                 idx++;
             }
 
-            int arrLength = leftList.length;
+            int arrLength = arr1.length;
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
             for (int i = 0; i< arrLength; i++){
-                if(hashMap.containsKey(leftList[i])){
-                    int similarity = leftList[i]*hashMap.get(leftList[i]);
-                    total += similarity;
+
+                // solve 1a
+                int distance = Math.abs(arr1[i] - arr2[i]);
+                total1a += distance;
+
+                // solve 1b
+                if(hashMap.containsKey(arr1[i])){
+                    int similarity = arr1[i]*hashMap.get(arr1[i]);
+                    total1b += similarity;
                 }
             }
-            System.out.println(total);
+
+            System.out.println("1a: " + total1a);
+            System.out.println("1b: " + total1b);
         } catch (IOException e) {
             e.printStackTrace();
         }
